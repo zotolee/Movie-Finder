b4ddab3e4ab09bd1c464c5b61295dc2077083e64
zotolee <akinayo711@gmail.com>
2023-07-10 15:31:23 +0100

    anoher commit

--- script.js
+++ script.js
@@ -27,15 +27,11 @@ function showMovies(movies) {
         movieEl.classList.add('movie')
 
         movieEl.innerHTML = `
-        <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-3">
-        <div class="col">
-        <div class="card mt-3">
+        <div class="card h-100 mt-3">
         <img src="${IMGPATH + poster_path}" class="card-img-top" alt="${title}">
         <div class="card-body">
           <p class="card-text">${overview}</p>
         </div>
-      </div>
-      </div>
       </div>
         `
 
